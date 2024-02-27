406a8a217c70aa820f9f7c96879ca1b24e9419f5
Steven Suddreth <stevensuddreth@gmail.com>
2024-02-27 12:32:42 -0500

    updates

--- server/db.js
+++ server/db.js
@@ -4,10 +4,10 @@ console.log(process.env)
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    user: DB_USER,
+    password: DB_PW,
     host: 'localhost',
-    port: process.env.DB_PORT, 
+    port: DB_PORT, 
     database: 'perntodo'
 });
 
